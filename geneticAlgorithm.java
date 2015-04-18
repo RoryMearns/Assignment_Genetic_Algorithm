1f8edda226b29999deff2ea41d82567d0d842efe
Rory Mearns <r.mearnsy@gmail.com>
2015-04-19 09:39:21 +1200

    created project files

---
+++ geneticAlgorithm.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Color;
+/**
+ * Genetic Algorithm Assignment for COSC 343
+ * 
+ * @author (Rory Mearns) 
+ * @version (19/4/2015)
+ */
+public class geneticAlgorithm
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class geneticAlgorithm
+     */
+    public geneticAlgorithm()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public static void main(String[] arguments) {
+        new geneticAlgorithm();
+    }
+}
